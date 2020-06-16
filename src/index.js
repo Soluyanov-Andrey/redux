bb3e646c1291f745cfbbc782dab2e29a05766f38
a.soluyanov <a--b--s@yandex.ru>
2020-06-16 14:50:44 +0400

    Добавили константу

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import {createStore} from 'redux'
+import rootReducer from "./redux/rootReducer";
+const store = createStore(rootReducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
