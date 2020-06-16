244c95cb28d2c57983099482febb95045d948ca6
a.soluyanov <a--b--s@yandex.ru>
2020-06-16 21:51:20 +0400

    вынесли полностью данные во вне

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import './App.scss'
 class App extends Component {
 
 
-  updateCounter(value) {
-    // this.setState({
-    //   counter: this.state.counter + value
-    // })
-  }
+
 
   render() {
       console.log('APP',this.props);
@@ -20,8 +16,8 @@ class App extends Component {
         <hr/>
 
         <div className="Actions">
-          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
-          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+          <button onClick={this.props.onAdd}>Добавить 1</button>
+          <button onClick={this.props.onSub}>Вычесть 1</button>
         </div>
       </div>
     )
@@ -32,5 +28,15 @@ function mapStateToProps(state) {
        counter: state.counter
   }
 }
+
+function mapDispatchToProps(dispatch){
+
+    return{
+        onAdd: ()=>dispatch({type:'ADD'}),
+        onSub: ()=>dispatch({type:'SUB'}),
+
+    }
+}
+
 // мы вызываем функцию она нам вернет новую функцию и в нее мы уже кладем компонент с которым мы хотим работать
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps,mapDispatchToProps)(App)
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,20 @@
 const initialState={
-    counter:100
+    counter:0
 }
 export default function  rootReducer(state = initialState,action){
+ switch (action.type) {
+
+     case 'ADD':
+         return {
+             counter: state.counter + 1
+         }
+     case 'SUB':
+         return {
+             counter: state.counter - 1
+         }
+
+ }
+
+
 return state
 }
