073a193d1671e1372bc2b816f6eb4aff3d2855c8
a.soluyanov <a--b--s@yandex.ru>
2020-06-17 17:49:50 +0400

    функция Middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 //В данный компонент мы должны обернуть все наше приложения для того чтоб указать что мы работаем с редаксом
 import {Provider} from 'react-redux';
 
 import rootReducer from "./redux/rootReducer";
 
-const store = createStore(rootReducer);
+//Миделвер это определенные функции которые добовляют определенный функционал чемуто
+//Можно записать так, длино а можно как ниже переписать
+// function loggerMiddleware(store){
+//     return function(next){
+//        return function(action){
+//          const result = next(action)
+//            console.log('Middleware', store.getState())
+//            return result
+//        }
+//     }
+// }
+// loggerMiddleware- функция которая при изменении store будет выводить все в консоль
+const loggerMiddleware = store => next =>action =>{
+    const result = next(action)
+          console.log('Middleware', store.getState())
+    return result
+}
+
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const app = (
     //передаем с помощью провайдера store
